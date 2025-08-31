4242fb1ed340626dbc6f0e57b53bb578c7f3a7a0
Abdulrahman Hamad <wadaln3ma@gmail.com>
2025-09-01 02:33:59 +0300

    chore(types): add local shims for @turf/length and polygon-to-line

---
+++ types/turf.d.ts
@@ -0,0 +1,43 @@
+// types/turf.d.ts
+
+// @turf/length
+declare module '@turf/length' {
+    import type {
+      Feature,
+      FeatureCollection,
+      LineString,
+      MultiLineString,
+      Polygon,
+      MultiPolygon,
+    } from 'geojson';
+  
+    type LengthInput =
+      | Feature<LineString | MultiLineString | Polygon | MultiPolygon>
+      | FeatureCollection<LineString | MultiLineString | Polygon | MultiPolygon>;
+  
+    const length: (
+      input: LengthInput,
+      options?: { units?: 'degrees' | 'radians' | 'miles' | 'kilometers' }
+    ) => number;
+  
+    export default length;
+  }
+  
+  // @turf/polygon-to-line
+  declare module '@turf/polygon-to-line' {
+    import type {
+      Feature,
+      FeatureCollection,
+      Polygon,
+      MultiPolygon,
+      LineString,
+      MultiLineString,
+    } from 'geojson';
+  
+    const polygonToLine: (
+      poly: Feature<Polygon | MultiPolygon> | Polygon | MultiPolygon
+    ) => Feature<LineString | MultiLineString> | FeatureCollection<LineString | MultiLineString>;
+  
+    export default polygonToLine;
+  }
+  
